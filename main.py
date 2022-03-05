fe3c3ce1279538a7e88cbce5c869ca0a0186417a
carymcorreia <carycorreia@mac.com>
2022-03-05 18:27:32 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+meal = input("What did you eat?")
+
+if meal =="cheese":
+    print("Danger Danger.  You are going to get high cholesterol and die")
+else:
+    print("Fine choice you will live forever")
